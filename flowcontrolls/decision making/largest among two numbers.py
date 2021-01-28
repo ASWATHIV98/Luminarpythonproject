51ebcc7aa5535bc3cac5f33914757f07568d99be
aswathi <aswathiv2016@gmail.com>
2021-01-28 11:19:28 +0530

    decision making program

---
+++ flowcontrolls/decision making/largest among two numbers.py
@@ -0,0 +1,6 @@
+num1=int(input("enter the first num1"))
+num2=int(input("enter the second num2"))
+if(num1>=num2):
+    print("num1 is largest")
+else:
+    print("num2 is largest")
---
+++ flowcontrolls/decision making/level_one.py
@@ -0,0 +1,19 @@
+# if...else
+
+#if(condition):
+    #stmt1
+
+
+#else:
+    #stmt2
+
+
+age=int(input("enter age"))
+if(age>=18):
+        print("u are eligible for voting")
+else:
+    print("u are not eligible for voting")
+
+
+
+
